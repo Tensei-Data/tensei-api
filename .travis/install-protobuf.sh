ae72ea21459f69b78ad9ba8af74b56456a02f35f
Jens Grassel <jens@wegtam.com>
2017-08-25 19:04:59 +0200

    Try to get protocol buffers running with travis.

--- .travis/install-protobuf.sh
+++ .travis/install-protobuf.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 set -e
 # check to see if protobuf folder is empty
-if [ ! -d "$HOME/protobuf/lib" ]; then
+if [ ! -d "$HOME/protobuf/bin" ]; then
   VER="3.4.0"
-  wget "https://github.com/google/protobuf/archive/v${VER}.tar.gz"
-  tar -xzvf "v${VER}.tar.gz"
-  cd "protobuf-${VER}" && ./configure --prefix=$HOME/protobuf && make && make install
+  wget "https://github.com/google/protobuf/releases/download/v3.4.0/protoc-${VER}-linux-x86_64.zip"
+  unzip -d "${HOME}/protobuf" "protoc-${VER}.zip"
 else
   echo "Using cached directory."
 fi
